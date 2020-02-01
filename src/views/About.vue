94a85b9c972e92631a5299e09a151b463a86375d
MPWEI <mailermpwei@gmail.com>
2020-02-01 12:26:00 +0800

    調整

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,22 +1,48 @@
 <template>
-  <ContentWrapper id="about">
-    <section class="container">
-      <h1>關於這裡</h1>
-      <p>你好</p>
-    </section>
-  </ContentWrapper>
+    <ContentWrapper id="about">
+        <section class="container">
+            <h1>關於這裡</h1>
+            <p>{{Post.Content}}</p>
+        </section>
+    </ContentWrapper>
 </template>
 
 <script>
-  export default {
-    name: 'About',
-    mounted() {
-      this.GetData()
-    },
-    methods: {
-      GetData() {
-        // console.log(this.$store)
-      }
+    export default {
+        name: 'About',
+        data() {
+            return {
+                Post: {}
+            }
+        },
+        mounted() {
+            this.$root.$Progress.start()
+            Promise.all(this.Init()).then(() => {
+                this.$root.$Progress.finish()
+            }).catch(_Error => {
+                alert(_Error)
+                this.$root.$Progress.fail()
+                this.$router.push('/notfound')
+            })
+        },
+        methods: {
+            Init() {
+                return [
+                    new Promise((_Resolve, _Reject) => this.GetData(_Resolve, _Reject)),
+                ]
+            },
+            GetData(_Resolve, _Reject) {
+                this.$store.state.database.collection('PostDetail').doc('about').get().then(_Response => {
+                    if (_Response.exists) {
+                        this.Post = _Response.data()
+                        _Resolve()
+                    } else {
+                        _Reject('Not found')
+                    }
+                }).catch((_Error) => {
+                    _Reject(_Error)
+                })
+            }
+        }
     }
-  }
 </script>
--- src/views/Post.vue
+++ src/views/Post.vue
@@ -1,23 +1,49 @@
 <template>
-  <ContentWrapper id="post">
-    <section class="container">
-      <h1>文章</h1>
-      {{$route.path}}
-      {{$route.params}}
-    </section>
-  </ContentWrapper>
+    <ContentWrapper id="post">
+        <section class="container">
+            <h1>文章</h1>
+            {{$route.path}}
+            {{$route.params}}
+        </section>
+    </ContentWrapper>
 </template>
 
 <script>
-  export default {
-    name: 'Post',
-    mounted() {
-      this.GetData()
-    },
-    methods: {
-      GetData() {
-        console.log(this.$route)
-      }
+    export default {
+        name: 'Post',
+        data() {
+            return {
+                Post: {}
+            }
+        },
+        mounted() {
+            this.$root.$Progress.start()
+            Promise.all(this.Init()).then(() => {
+                this.$root.$Progress.finish()
+            }).catch(_Error => {
+                alert(_Error)
+                this.$root.$Progress.fail()
+                this.$router.push('/notfound')
+            })
+        },
+        methods: {
+            Init() {
+                return [
+                    new Promise((_Resolve, _Reject) => this.GetData(_Resolve, _Reject)),
+                ]
+            },
+            GetData(_Resolve, _Reject) {
+                this.$store.state.database.collection('PostDetail').doc(this.$route.params.slug).get().then(_Response => {
+                    if (_Response.exists) {
+                        this.Post = _Response.data()
+                        _Resolve()
+                    } else {
+                        _Reject('Not found')
+                    }
+                }).catch((_Error) => {
+                    _Reject(_Error)
+                })
+            }
+        }
     }
-  }
 </script>
