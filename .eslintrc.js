609861f5184cc60b405e4ccd61916374fe6c27b6
Yao <apollord.yao@gmail.com>
2020-02-14 21:48:10 +0800

    調整eslint設定

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,7 +10,7 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-unused-vars': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-unused-vars': process.env.NODE_ENV === 'off',
   },
   parserOptions: {
     parser: 'babel-eslint'
